ccd35678949faca5f93d3c0b80a41de353d53973
Taniele <127350142+TanyRM@users.noreply.github.com>
2023-05-12 12:43:25 -0300

    Update Uniao.java

--- t07/t07/Uniao.java
+++ t07/t07/Uniao.java
@@ -1,5 +1,16 @@
 package t07;
 
-public class Uniao {
+import java.util.ArrayList;
 
+public class Uniao {
+	private ArrayList filhos;
+	private Pessoa parceiros[2];
+	
+	public void novoFilho(Pessoa p) {
+		filhos.add(p);
+	}
+	public Uniao(Pessoa a, Pessoa b) {
+		parceiros[0] = a;
+		parceiros[1] = b;
+	}
 }
