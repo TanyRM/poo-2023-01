7d56ee2178e6e2f7c122e3636f5f40a6b29d6464
Taniele <127350142+TanyRM@users.noreply.github.com>
2023-06-15 11:30:03 -0300

    Delete Casa.java

--- t11/src/main/mc01/Casa.java
+++
@@ -1,9 +0,0 @@
-package t11.src.main.mc01;
-
-public class Casa {
-    private Area[] areas;
-    private Banheiro[] banheiros;
-    private Garagem[] garagems;
-    private Quarto[] quartos;
-    private Sala[] salas;
-}
