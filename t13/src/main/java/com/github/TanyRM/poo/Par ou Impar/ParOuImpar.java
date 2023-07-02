d95bf05bc7460f6f833e76ba877020d5efd7b96d
TanyRM <tanielemadu@outlook.com>
2023-07-02 11:10:07 -0300

    Arquivos adicionados

---
+++ t13/src/main/java/com/github/TanyRM/poo/Par ou Impar/ParOuImpar.java
@@ -0,0 +1,12 @@
+public class ParOuImpar {
+    public static void main(String[] args) {
+		int valor = 49;
+		
+		if (valor%2 == 0) {
+			System.out.println("O numero inserido e par!");
+		}
+		else {
+			System.out.println("O numero inserido e impar!");
+		}
+	}
+}
